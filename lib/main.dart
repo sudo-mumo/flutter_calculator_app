9254cd909aaf86f280b5433982f8c9ff949ac1a2
sudo-mumo <thomas.mumo@students.jkuat.ac.ke>
2022-11-12 00:31:05 +0300

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: const HomeScreen(),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: const Color.fromARGB(234, 17, 11, 11),
+    );
   }
 }
