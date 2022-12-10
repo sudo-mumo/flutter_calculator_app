20a0ebc42d231025e0c0bd5fe3c0fdb272310730
sudo-mumo <thomas.mumo@students.jkuat.ac.ke>
2022-12-10 15:04:44 +0300

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,8 +10,27 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(204, 17, 11, 11),
-    );
+    return Scaffold(
+        body: Container(
+      color: Colors.black12,
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(12, 12, 12, 12),
+                  border: Border.all(color: Colors.red),
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      topRight: Radius.circular(12))),
+              color: const Color.fromARGB(66, 148, 141, 141),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
